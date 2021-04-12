3aa3d8c4cb885b09b8b88af7112b17504beef89d
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-04-12 21:51:45 +1000

    Completed function to render weather data to html

--- assets/script.js
+++ assets/script.js
@@ -36,13 +36,65 @@ function getSaveHistory() {
     return searchHistory;
 }
 
-function renderWeatherData(data) {
+function renderWeatherData(data, displayName) {
     for (var i = 0; i < 7; i++) {
         if (i===0) {
             var todayEl = document.getElementById("weather-today");
-            var text = document.createTextNode(data.current.temp);
-            todayEl.appendChild(text)
+
+            var headerEl = todayEl.getElementsByClassName("card-title")[0];
+            headerEl.textContent = "";
+
+            var headerText = displayName + ", "+ moment.unix(data.current.dt).format("DD/MM/YYYY");
+
+            var headerNode = document.createTextNode(headerText);
+
+            var icon = document.createElement("img");
+            icon.setAttribute("src", "http://openweathermap.org/img/wn/"+ data.current.weather[0].icon +"@2x.png");
+            headerEl.appendChild(headerNode);
+            headerEl.appendChild(icon);
+
+            //Render general weather info
+            var info = "Temperature: "+ data.current.temp+ "°C "+ "Wind Speed: " +data.current.wind_speed+ "KM/H "+ "Humidity: "+ data.current.humidity+ "%";
+            var infoNode = document.createTextNode(info);
+            var infoEl = todayEl.getElementsByClassName("card-text")[0];
+            infoEl.textContent = "";
+            infoEl.appendChild(infoNode);
+
+            //Render UV Index
+            var uvIndex = document.createTextNode(data.current.uvi);
+            continue;
         } 
+     
+        var dayCard = document.getElementsByClassName("forecast-card")[i-1];
+
+        var cardTitle = dayCard.getElementsByClassName("card-title")[0];
+        cardTitle.textContent = "";
+
+        var titleText = moment.unix(data.daily[i].dt).format("DD/MM/YY");
+
+        var titleNode = document.createTextNode(titleText);
+
+        var cardIcon = document.createElement("img");
+        cardIcon.setAttribute("src", "http://openweathermap.org/img/wn/"+ data.daily[i].weather[0].icon +"@2x.png");
+        cardTitle.appendChild(titleNode);
+        cardTitle.appendChild(cardIcon);
+
+        var cardInfoEl = dayCard.getElementsByClassName("card-text")[0];
+        cardInfoEl.textContent = "";
+
+        var tempNode = document.createTextNode("Temp: "+ data.daily[i].temp.day+ "°C \n");
+        cardInfoEl.appendChild(tempNode);
+        cardInfoEl.appendChild(document.createElement("hr"));
+
+        var windNode = document.createTextNode("Wind Speed: " +data.daily[i].wind_speed+ "KM/H \n");
+        cardInfoEl.appendChild(windNode);
+        cardInfoEl.appendChild(document.createElement("hr"));
+
+        var humidityNode = document.createTextNode("Humidity: "+ data.daily[i].humidity+ "%");
+        cardInfoEl.appendChild(humidityNode);
+
+        //Render UV Index
+        var uvIndex = document.createTextNode(data.daily[i].uvi);
     }
 }
 
@@ -67,7 +119,7 @@ document.getElementById("search").addEventListener("submit", function(e) {
                 .then(data=>{return data.json()})
                 .then(res=>{
                     console.log(res);
-                    renderWeatherData(res);
+                    renderWeatherData(res, coordRes[0].display_name.split(",")[0]);
                 });
         });
 });
